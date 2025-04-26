34e11305773ba40ca66dcabba725dc9a3ea981bf
Jiangbo Shen <js12154@nyu.edu>
2025-04-25 21:29:46 -0400

    delete ai container

--- SWE5/ai-container/run.py
+++
@@ -1,9 +0,0 @@
-# ai-container/run.py
-from app import create_app, socketio
-from app.socket_events import *  # Import all socket event handlers
-
-app = create_app()
-
-if __name__ == '__main__':
-    # Run the app with Socket.IO
-    socketio.run(app, host='0.0.0.0', port=5002, debug=True)
